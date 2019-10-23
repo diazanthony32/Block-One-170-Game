1e7f9a1b5de133511d07b5d7a3aabc6f16c045a3
Anthony Diaz <diazanthony32@gmail.com>
2019-10-22 22:57:09 -0700

    Cube turning
    -added cube turning fuctionality
        * has animation/variable speed
        * has self correcting function for
    turning overshoots
    - code looks gross

--- Assets/Scripts/objectClicker.cs
+++ Assets/Scripts/objectClicker.cs
@@ -7,11 +7,15 @@ public class objectClicker : MonoBehaviour
 
 	public GameObject block;
 
-	// float tiltAroundX = 0.0f;
-	// float tiltAroundY = 0.0f;
-	// float tiltAroundZ = 0.0f;
+	float tiltAroundX = 0.0f;
+	float tiltAroundY = 0.0f;
+	float tiltAroundZ = 0.0f;
 
-	// float smooth = 6.0f;
+	float turnSpeed = 2.5f;
+
+	float totalTurnX;
+	float totalTurnY;
+	float totalTurnZ;
 
 
     // Start is called before the first frame update
@@ -45,12 +49,98 @@ public class objectClicker : MonoBehaviour
 	    }
 
 	    //Get the Renderer component from the new cube
-	   	// var blockTransformer = block.GetComponent<Transform>();
+	   	var blockTransformer = block.GetComponent<Transform>();
 
-	    // Quaternion target = Quaternion.Euler(tiltAroundX, tiltAroundY, tiltAroundZ);
+	    //Quaternion target = Quaternion.Euler(tiltAroundX, tiltAroundY, tiltAroundZ);
 
      //    // Dampen towards the target rotation
-     //    blockTransformer.transform.rotation = Quaternion.Slerp(blockTransformer.transform.rotation, target,  Time.deltaTime * smooth);
+        //blockTransformer.transform.rotation = Quaternion.Slerp(blockTransformer.transform.rotation, target,  Time.deltaTime * smooth);
+        blockTransformer.transform.Rotate(new Vector3(tiltAroundX, tiltAroundY, tiltAroundZ) * Time.deltaTime * turnSpeed, Space.World);
+
+        //print(new Vector3(tiltAroundX, tiltAroundY, tiltAroundZ) * Time.deltaTime * turnSpeed);
+
+        totalTurnX += (tiltAroundX * Time.deltaTime * turnSpeed);
+        //print("X: " + totalTurnX);
+
+        if(Mathf.Abs(totalTurnX) > 90){
+        	
+        	print("Overshot X by: " + (Mathf.Abs(totalTurnX) - 90));
+        	var Overshot = (Mathf.Abs(totalTurnX) - 90);
+
+        	if(tiltAroundX < 0)
+        	{
+        		blockTransformer.transform.Rotate(new Vector3(Overshot, 0, 0), Space.World);
+        	}
+        	else
+        	{
+        		blockTransformer.transform.Rotate(new Vector3(-Overshot, 0, 0), Space.World);
+
+        	}
+
+        	tiltAroundX = 0;
+        	totalTurnX = 0;
+        }
+
+        totalTurnY += (tiltAroundY * Time.deltaTime * turnSpeed);
+        //print("Y: " + totalTurnY);
+
+        if(Mathf.Abs(totalTurnY) > 90){
+
+        	print("Overshot Y by: " + (Mathf.Abs(totalTurnY) - 90));
+        	var Overshot = (Mathf.Abs(totalTurnY) - 90);
+
+        	if(tiltAroundY < 0)
+        	{
+        		blockTransformer.transform.Rotate(new Vector3(0, Overshot, 0), Space.World);
+        	}
+        	else
+        	{
+        		blockTransformer.transform.Rotate(new Vector3(0, -Overshot, 0), Space.World);
+
+        	}
+
+        	//blockTransformer.transform.Rotate(new Vector3(0, -Overshot, 0), Space.World);
+
+        	tiltAroundY = 0;
+        	totalTurnY = 0;
+        }
+
+        totalTurnZ += (tiltAroundZ * Time.deltaTime * turnSpeed);
+        //print("Z: " + totalTurnZ);
+
+        if(Mathf.Abs(totalTurnZ) > 90){
+
+        	print("Overshot Z by: " + (Mathf.Abs(totalTurnZ) - 90));
+        	var Overshot = (Mathf.Abs(totalTurnZ) - 90);
+
+        	if(tiltAroundZ < 0)
+        	{
+        		blockTransformer.transform.Rotate(new Vector3(0, 0, Overshot), Space.World);
+        	}
+        	else
+        	{
+        		blockTransformer.transform.Rotate(new Vector3(0, 0, -Overshot), Space.World);
+
+        	}
+
+        	//blockTransformer.transform.Rotate(new Vector3(0, 0, -Overshot), Space.World);
+
+        	tiltAroundZ = 0;
+        	totalTurnZ = 0;
+        }
+
+        // if(blockTransformer.transform.rotation.x)
+        // {
+        // 	tiltAroundX = 0;
+        // }
+
+        // if(blockTransformer.transform.rotation.x == tiltAroundX){
+        // 	print("HI");
+        // }
+
+        //print(Mathf.Abs(blockTransformer.transform.rotation.x * 180));
+
+        //print("(" + tiltAroundX + ", " + tiltAroundY + ", " + tiltAroundZ + ")");
 
     }
 
@@ -78,8 +168,8 @@ public class objectClicker : MonoBehaviour
 	       	var blockTransformer = block.GetComponent<Transform>();
 
 			//Call SetColor using the shader property name "_Color" and setting the color to red
-			blockTransformer.transform.Rotate(90,0,0,Space.World);
-			// tiltAroundX += 90;
+			// blockTransformer.transform.Rotate(90,0,0,Space.World);
+			tiltAroundX = 90;
 	    }
 
 	    if(plane.gameObject.name == "DownLeft")
@@ -88,9 +178,9 @@ public class objectClicker : MonoBehaviour
 	       	var blockTransformer = block.GetComponent<Transform>();
 
 			//Call SetColor using the shader property name "_Color" and setting the color to red
-			blockTransformer.transform.Rotate(-90,0,0,Space.World);
+			// blockTransformer.transform.Rotate(-90,0,0,Space.World);
 
-			// tiltAroundX += -90;
+			tiltAroundX = -90;
 	    }
 	    
 	    //------- DONE
@@ -101,9 +191,9 @@ public class objectClicker : MonoBehaviour
 	       	var blockTransformer = block.GetComponent<Transform>();
 
 			// //Call SetColor using the shader property name "_Color" and setting the color to red
-			blockTransformer.transform.Rotate(0,0,90,Space.World);
+			// blockTransformer.transform.Rotate(0,0,90,Space.World);
 
-			// tiltAroundZ += 90;
+			tiltAroundZ = 90;
 	    }
 
 	    if(plane.gameObject.name == "DownRight")
@@ -112,9 +202,9 @@ public class objectClicker : MonoBehaviour
 	       	var blockTransformer = block.GetComponent<Transform>();
 
 			// //Call SetColor using the shader property name "_Color" and setting the color to red
-			blockTransformer.transform.Rotate(0,0,-90,Space.World);
+			// blockTransformer.transform.Rotate(0,0,-90,Space.World);
 
-			// tiltAroundZ += -90;
+			tiltAroundZ = -90;
 	    }
 
 	    //--------- DONE
@@ -125,8 +215,8 @@ public class objectClicker : MonoBehaviour
 	       	var blockTransformer = block.GetComponent<Transform>();
 
 			// //Call SetColor using the shader property name "_Color" and setting the color to red
-			blockTransformer.transform.Rotate(0,90,0,Space.World);
-			// tiltAroundY += 90;
+			// blockTransformer.transform.Rotate(0,90,0,Space.World);
+			tiltAroundY = 90;
 	    }
 
 	    if(plane.gameObject.name == "RightTurn")
@@ -135,17 +225,19 @@ public class objectClicker : MonoBehaviour
 	       	var blockTransformer = block.GetComponent<Transform>();
 
 			//Call SetColor using the shader property name "_Color" and setting the color to red
-			blockTransformer.transform.Rotate(0,-90,0,Space.World);
+			// blockTransformer.transform.Rotate(0,-90,0,Space.World);
 
-	       	// tiltAroundY += -90;
+	       	tiltAroundY = -90;
 
 			// Quaternion target = Quaternion.Euler(0, -90, 0);
 
-   //      	// Dampen towards the target rotation
-   //      	blockTransformer.transform.rotation = Quaternion.Slerp(blockTransformer.transform.rotation, target,  Time.deltaTime * 6.0f);
+   		//      	// Dampen towards the target rotation
+   		//      	blockTransformer.transform.rotation = Quaternion.Slerp(blockTransformer.transform.rotation, target,  Time.deltaTime * 6.0f);
 	    }
 
 
     }
+
+
 
 }
